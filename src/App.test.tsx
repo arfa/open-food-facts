cf1197ac1682ab7a8f6435a29b0f50a2e0ac43ca
Marwen Arfa <marwen.arfa@gmail.com>
2021-09-22 01:39:27 +0200

    chore: setup tailwind css, daisyui

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Hello there title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const element = screen.getByText(/Hello there/i);
+  expect(element).toBeInTheDocument();
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='hero min-h-screen bg-base-200'>
+      <div className='text-center hero-content'>
+        <div className='max-w-md'>
+          <h1 className='mb-5 text-5xl font-bold'>Hello there</h1>
+          <p className='mb-5'>
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi
+            exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.
+          </p>
+          <button className='btn btn-primary'>Get Started</button>
+        </div>
+      </div>
     </div>
   );
 }
